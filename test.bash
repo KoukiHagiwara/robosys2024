7a1639e372a7d31937d464f25b3f1afd8982763c
Kouki Hagiwara <kouki-abc-1108@outlook.jp>
2024-11-20 11:38:19 +0900

    factorialを追加

--- test.bash
+++ test.bash
@@ -19,6 +19,33 @@ out=$(echo | ./plus)
 [ "${out}" = "" ] || ng "$LINENO"
 
 
+
+out=$(./factorial 0)
+[ "${out}" = "1" ] || ng "$LINENO"
+
+out=$(./factorial 1)
+[ "${out}" = "1" ] || ng "$LINENO"
+
+out=$(./factorial 5)
+[ "${out}" = "120" ] || ng "$LINENO"
+
+out=$(./factorial 7)
+[ "${out}" = "5040" ] || ng "$LINENO"
+
+
+out=$(./factorial )
+[ "$?" = 1 ]      || ng "$LINENO"
+[ "${out}" = "" ] || ng "$LINENO"
+
+out=$(./factorial hello ) #2>/dev/null)
+[ "$?" = 1 ] || ng "$LINENO"
+[ "${out}" = "" ] || ng"$LINENO"
+
+out=$(./factorial)  #2>/dev/null)
+[ "$?" = 1 ] || ng "$LINENO"
+[ "${out}" = "" ] || ng"$LINENO"
+
+
 [ "${res}" = 0 ] && echo OK
 exit $res
 
